11faae84bffc52d44c6c01977efe8e7c14588299
pilajes <61300686+pilajes@users.noreply.github.com>
2024-04-22 00:44:10 -0400

    minor fix

--- src/components/sections/index/optional/career.tsx
+++ src/components/sections/index/optional/career.tsx
@@ -37,7 +37,7 @@ export default function Career() {
 						</div>
 						<div className={career.companyAlt}></div>
 
-						<Badges list={fullStack} block="stack" fullContainer="fullContainer" color={undefined}/>
+						<Badges list={fullStack} block="stack" fullContainer="fullContainer" />
 						</div>
 
 					{/*	<article className={career.companyPositions}>
